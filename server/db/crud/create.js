78f1441aab7c6766b5e131fa2ee66a10d36217b2
kylestech95 <kylestech95@penguin>
2024-07-17 21:37:38 -0400

    feat: split psql & mysql usage

--- server/db/crud/create.js
+++ server/db/crud/create.js
@@ -1,13 +1,62 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+const mysql = require("../db.js").mysqlObj;
+const { connection, pool } = require("../db.js").mysqlObj;
+const pg = require("../db.js").pool;
 
+// middleware
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+router.use(express.static("client/test"));
 
-
-router.route('/').get((req,res)=>{
-    res.json({data:'create'})
-})
-
-
-
+// create a user with mysql
+router.route("/mysql/spawn").post(async (req, res) => {
+  const { display_name, email, gender } = req.body;
+  // are users existent in the db?
+  await fetch("http://localhost:9934/read/mysql/review/users")
+    .then((r) => r.json())
+    .then((d) => {
+      if (!d.users) res.json({ err: "no data present" });
+      let userFound = [...d.users].filter((user, index) => {
+        return user.email === email;
+      });
+      if (userFound.length > 0) {
+        console.log("user is found");
+        res.send("user is found in db");
+      } else {
+        connection.query(
+          "insert into users(display_name,email,gender) values(?,?,?)",
+          [display_name, email, gender],
+          (err, result) => {
+            return err ? console.log(err) : res.send("user created!");
+          }
+        );
+      }
+    });
+});
+// create a user with psql
+router.route("/psql/spawn").post(async (req, res) => {
+  const { user_id, display_name, email, gender } = req.body;
+  await fetch("http://localhost:9934/read/psql/review/users")
+    .then((r) => r.json())
+    .then(async (d) => {
+      if (!d.users) res.json({ err: "no data present" });
+      let userFound = [...d.users].filter((user, index) => {
+        return user.email === email;
+      });
+      if (userFound.length > 0) {
+        console.log("user is found");
+        res.send("user is found in db");
+      } else {
+        let user_inserted = await pg.query(
+          "insert into users(display_name,email,gender) values($1,$2,$3)",
+          [display_name, email, gender]
+        );
+        console.log(user_inserted);
+        res.send('user added')
+        
+      }
+    });
+});
 
 module.exports = router;
--- server/db/crud/read.js
+++ server/db/crud/read.js
@@ -1,13 +1,44 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+const { connection, pool } = require("../db.js").mysqlObj;
+const pg = require("../db.js").pool;
 
+// middleware
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+router.use(express.static("client/test"));
 
+// read user/scores data - mysql
+router.route("/mysql/review/:data").get((req, res) => {
+  const { data } = req.params;
+  if (/scores/i.test(data)) {
+    let scores = connection.query("select * from scores", (err, result) => {
+      return err ? console.log(err) : res.json({ scores: result });
+    });
+  } else if (/users/i.test(data)) {
+    let users = connection.query("select * from users", (err, result) => {
+      return err ? console.log(err) : res.json({ users: result });
+    });
+  } else {
+    res.send("not recognized");
+  }
+});
 
-router.route('/').get((req,res)=>{
-    res.json({data:'read'})
-})
-
-
-
-
+// read user/scores data - psql
+router.route("/psql/review/:data").get(async (req, res) => {
+  const { data } = req.params;
+  if (/scores/i.test(data)) {
+    let scores = await pg.query("select * from scores");
+    return !scores.rows
+      ? new Error("wrong input")
+      : res.json({ scores: scores.rows });
+  } else if (/users/i.test(data)) {
+    let users = await pg.query("select * from users");
+    return !users.rows
+      ? new Error("wrong input")
+      : res.json({ users: users.rows });
+  } else {
+    res.send("not recognized");
+  }
+});
 module.exports = router;
