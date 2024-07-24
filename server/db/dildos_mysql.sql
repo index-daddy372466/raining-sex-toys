6d6870bfd77a12e5300b99878a5ef12fb0320e62
kylestech95 <kylestech95@penguin>
2024-07-24 08:06:35 -0400

    fork: database column removal

--- server/db/dildos_mysql.sql
+++ server/db/dildos_mysql.sql
@@ -33,7 +33,6 @@ DROP TABLE IF EXISTS `scores`;
 CREATE TABLE `scores` (
   `score_id` int(11) NOT NULL AUTO_INCREMENT,
   `best` int(11) NOT NULL,
-  `current` int(11) NOT NULL,
   `average` int(11) NOT NULL,
   `u_id` int(11) NOT NULL,
   PRIMARY KEY (`score_id`),
@@ -63,7 +62,7 @@ CREATE TABLE `users` (
   `email` varchar(319) NOT NULL,
   `password` varchar(120) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,6 +71,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'ben','ben@ben','$2b$11$U2mmvYsRuQbXP55YGj3qx.TuHqYa/05MVuGOvqehCFBrXYU82R37a');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -84,4 +84,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-19 19:17:13
+-- Dump completed on 2024-07-24  8:04:53
