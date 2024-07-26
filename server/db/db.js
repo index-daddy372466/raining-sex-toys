3cd1e89071db20880a0666920b131ea1dcdd9293
kylestech95 <kylestech95@penguin>
2024-07-26 15:59:33 -0400

    test: sequelize

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,17 @@
 const Pool = require("pg-pool");
 require("dotenv").config();
+const Sequelize = require("sequelize").Sequelize;
+
+let sequelize;
+if (process.env.URI) {
+  sequelize = new Sequelize(process.env.URI);
+} else {
+  sequelize = new Sequelize(process.env.DB, process.env.DBU, process.env.DBPD,  {
+    host: process.env.DBH,
+    dialect: "postgres",
+    ssl: true,
+  });
+}
 
 // psql db
 const pool = new Pool({
@@ -9,6 +21,5 @@ const pool = new Pool({
   port: process.env.DBP,
   password: process.env.DBPD,
 });
-
 
 module.exports = { pool };
