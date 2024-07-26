1e5272e393e43f57b207ced5ab5cdbb006ff2792
kylestech95 <kylestech95@penguin>
2024-07-26 10:18:05 -0400

    fix: registration

--- server/db/crud/create.js
+++ server/db/crud/create.js
@@ -15,27 +15,22 @@ router.route("/register").get((req, res) => {
 // register a user with psql
 router.route("/psql/register").post(async (req, res) => {
   const { display_name, email, password } = req.body;
-  await fetch("http://localhost:9934/read/psql/review/users")
-    .then((r) => r.json())
-    .then(async (d) => {
-      if (!d.users) res.json({ err: "no data present" });
-      let userFound = [...d.users].filter((user, index) => {
-        return user.email === email;
-      });
-      if (userFound.length > 0) {
-        console.log("user is found");
-        res.send("user is found in db");
-      } else {
-        let salt = 11;
-        let hash = await bcrypt.hash(password, salt);
-        let user_inserted = await pg.query(
-          "insert into users(display_name,email,password) values($1,$2,$3)",
-          [display_name, email, hash]
-        );
-        console.log(user_inserted);
-        res.send("user added");
-      }
-    });
+  let users = await pg.query("select email from users where email=$1", [
+    email,
+  ]);
+  let uFound = users.rows.filter((x) => x.email === email);
+  if (uFound.length > 0) {
+    console.log("user is found");
+    res.send("user is found in db");
+  } else {
+    let salt = 11;
+    let hash = await bcrypt.hash(password, salt);
+    let user_inserted = await pg.query(
+      "insert into users(display_name,email,password) values($1,$2,$3)",
+      [display_name, email, hash]
+    );
+    res.redirect('/login')
+  }
 });
 
 module.exports = router;
