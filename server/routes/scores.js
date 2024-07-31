fb71f234713c97bad9e53931d809977d94ee2b08
kylestech95 <kylestech95@penguin>
2024-07-31 11:19:28 -0400

    chore: update frontend (scores/contact)

--- server/routes/scores.js
+++ server/routes/scores.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-
+const {GetScores} = require('../db/commands.js')
 router.use(express.json());
 
 router.route("/").get((req, res) => {
-  //   res.send("welcome to contact");
-  res.render('contact.ejs')
-  });
+//   res.send("welcome to scores");
+res.render('scores.ejs')
+});
+
+
+
+
 
 module.exports = router;
