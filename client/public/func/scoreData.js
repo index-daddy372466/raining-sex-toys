13090bdbcc0af6ad64aee5cc02ef41cc587706ec
kylestech95 <kylestech95@penguin>
2024-08-01 07:47:29 -0400

    feat: organize scores

--- client/public/func/scoreData.js
+++ client/public/func/scoreData.js
@@ -1,6 +1,29 @@
-export default async function scoreData(){
-    let scores = await fetch('/scores/all').then(r=>r.json()).then(d=>{
-        console.log(d)
-    })
-    return scores;
+const token = await fetch("/game/token")
+  .then((r) => r.json())
+  .then((d) => d.token);
+const condition = {};
+condition.best = "best";
+condition.worst = "worst";
+
+let id = token.identity;
+// console.log(id)
+export default async function scoreData() {
+  // all scores
+  let scores = await fetch("/scores/all")
+      .then((r) => r.json())
+      .then((d) => d.data),
+    // scores by current user
+    userScores = await fetch(`/scores/users/${id}`)
+      .then((r) => r.json())
+      .then((d) => d.data),
+    // best score
+    bestScore = await fetch(`/scores/spectrum/${id}/${condition.best}`)
+      .then((r) => r.json())
+      .then((d) => d.data),
+    // worst score
+    worstScore = await fetch(`/scores/spectrum/${id}/${condition.worst}`)
+      .then((r) => r.json())
+      .then((d) => d.data);
+
+  return { all: scores, user: userScores, best: bestScore, worst: worstScore };
 }
--- client/public/score.js
+++ client/public/score.js
@@ -1,3 +1,4 @@
 import scoreData from "./func/scoreData.js"
 // scores page - send scores data
-await scoreData()
+let scores = await scoreData()
+console.log(scores)
