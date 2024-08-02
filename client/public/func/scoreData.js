7df005d9729325dc917c916f9282a7fbac95eb18
kylestech95 <kylestech95@penguin>
2024-08-02 19:29:27 -0400

    test: waterfall on scoreData

--- client/public/func/scoreData.js
+++ client/public/func/scoreData.js
@@ -6,24 +6,40 @@ condition.best = "best";
 condition.worst = "worst";
 
 let id = token.identity;
+// all scores
+let scores = await fetch("/scores/all")
+.then((r) => r.json())
+.then((d) => d.data),
+// scores by current user
+userScores = await fetch(`/scores/users/${id}`)
+.then((r) => r.json())
+.then((d) => d.data),
+// best score
+bestScore = await fetch(`/scores/spectrum/${id}/${condition.best}`)
+.then((r) => r.json())
+.then((d) => d.data),
+// worst score
+worstScore = await fetch(`/scores/spectrum/${id}/${condition.worst}`)
+.then((r) => r.json())
+.then((d) => d.data);
 // console.log(id)
 export default async function scoreData() {
-  // all scores
-  let scores = await fetch("/scores/all")
-      .then((r) => r.json())
-      .then((d) => d.data),
-    // scores by current user
-    userScores = await fetch(`/scores/users/${id}`)
-      .then((r) => r.json())
-      .then((d) => d.data),
-    // best score
-    bestScore = await fetch(`/scores/spectrum/${id}/${condition.best}`)
-      .then((r) => r.json())
-      .then((d) => d.data),
-    // worst score
-    worstScore = await fetch(`/scores/spectrum/${id}/${condition.worst}`)
-      .then((r) => r.json())
-      .then((d) => d.data);
+  // // all scores
+  // let scores = await fetch("/scores/all")
+  //     .then((r) => r.json())
+  //     .then((d) => d.data),
+  //   // scores by current user
+  //   userScores = await fetch(`/scores/users/${id}`)
+  //     .then((r) => r.json())
+  //     .then((d) => d.data),
+  //   // best score
+  //   bestScore = await fetch(`/scores/spectrum/${id}/${condition.best}`)
+  //     .then((r) => r.json())
+  //     .then((d) => d.data),
+  //   // worst score
+  //   worstScore = await fetch(`/scores/spectrum/${id}/${condition.worst}`)
+  //     .then((r) => r.json())
+  //     .then((d) => d.data);
 
   return { all: scores, user: userScores, best: bestScore, worst: worstScore };
 }
