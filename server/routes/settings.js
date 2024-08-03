3ad7b552fdfdea4ebea99a143183a795d7faa44a
kylestech95 <kylestech95@penguin>
2024-08-03 13:24:02 -0400

    Initial commit

---
+++ server/routes/settings.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {checkAuthenticated} = require('../lib/auth.config')
+
+router.use(express.json());
+router.use(checkAuthenticated)
+
+router.route("/").get((req, res) => {
+//   res.send("welcome to settings");
+// console.log(req.session)
+console.log(req.session.passport.user.display_name)
+let username = req.session.passport.user.display_name;
+res.render('settings.ejs',{
+user:username.toUpperCase(),
+isAuthenticated:req.isAuthenticated(),
+})
+});
+
+module.exports = router;
