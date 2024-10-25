101a5c9b523fcdb84c7f1d277ed51b854862073e
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-10-25 15:20:41 -0400

    test: config mysql

--- server/db/db.js
+++ server/db/db.js
@@ -1,19 +1,47 @@
 const Pool = require("pg-pool");
+// const mysql = require('mysql2');
 require("dotenv").config();
 const Sequelize = require("sequelize").Sequelize;
 
 let sequelize;
+// if (process.env.URI) {
+//   sequelize = new Sequelize(process.env.URI);
+// } else {
+//   sequelize = new Sequelize(process.env.DB, process.env.DBU, process.env.DBPD,  {
+//     host: process.env.DBH,
+//     dialect: "postgres",
+//     ssl: true,
+//   });
+// }
+
+// // mysql
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//   dialect: 'mysql',
+//   dialectOptions: {
+//     // Your mysql2 options here
+//   },
+// });
+
+// mysql
 if (process.env.URI) {
   sequelize = new Sequelize(process.env.URI);
 } else {
   sequelize = new Sequelize(process.env.DB, process.env.DBU, process.env.DBPD,  {
     host: process.env.DBH,
-    dialect: "postgres",
+    dialect: "mysql",
     ssl: true,
   });
 }
 
-// psql db
+// pool-db
+// const pool = new mysql.createConnection({
+//   database: process.env.DB,
+//   user: process.env.DBU,
+//   host: process.env.DBH,
+//   port: process.env.DBP,
+//   password: process.env.DBPD,
+// })
+
 const pool = new Pool({
   database: process.env.DB,
   user: process.env.DBU,
